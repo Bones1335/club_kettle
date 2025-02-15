0bf8a96af76f1b37f05cdef80ec3975ffa4f1e9d
Alexander Hansen <ahansen1335@gmail.com>
2025-02-15 07:44:16 +0100

    update test script for exercises create endpoint

--- test.sh
+++ test.sh
@@ -14,4 +14,10 @@ user_user=$(echo $JSON_POST1 | jq -r .username)
 url="http://localhost:8080/api/users/$user_id"
 
 echo $url
-curl -X PUT $url -H "Content-Type: application/json" -d "{\"id\":\"$user_id\",\"email\":\"john.snow83@gmail.com\",\"last_name\":\"$user_last\",\"first_name\":\"$user_first\",\"username\":\"$user_user\"}"
+JSON_PUT1=$(curl -X PUT $url -H "Content-Type: application/json" -d "{\"id\":\"$user_id\",\"email\":\"john.snow83@gmail.com\",\"last_name\":\"$user_last\",\"first_name\":\"$user_first\",\"username\":\"$user_user\"}")
+
+echo $JSON_PUT1 | jq .
+
+JSON_POST2=$(curl -X POST http://localhost:8080/api/exercises -H "Content-Type:appication/json" -d "{\"name\":\"inside circle\",\"tool\":\"clubbell\",\"user_id\":\"$user_id\"}")
+
+echo $JSON_POST2 | jq .
