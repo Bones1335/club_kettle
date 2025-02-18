370d00ffc8121a24c9fee7eb3a7ff901f574025e
Alexander Hansen <ahansen1335@gmail.com>
2025-02-18 06:15:48 +0100

    write curl test for update exercise endpoint

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This API allows the user to interact with a Postgresql database to track and sto
  - [ ] DELETE user info
  - [x] CREATE exercise endpoint where the user can create various exercises
  - [x] Endpoint to GET all created exercises by a user
- - [ ] UPDATE an exercise
+ - [x] UPDATE an exercise
  - [x] DELETE an exercise
  - [ ] CREATE workout with registered exercises
  - [ ] GET full workout
--- test.sh
+++ test.sh
@@ -11,10 +11,10 @@ user_last=$(echo $JSON_POST1 | jq -r .last_name)
 user_first=$(echo $JSON_POST1 | jq -r .first_name)
 user_user=$(echo $JSON_POST1 | jq -r .username)
 
-url="http://localhost:8080/api/users/$user_id"
+url1="http://localhost:8080/api/users/$user_id"
 
-echo $url
-JSON_PUT1=$(curl -X PUT $url -H "Content-Type: application/json" -d "{\"id\":\"$user_id\",\"email\":\"john.snow83@gmail.com\",\"last_name\":\"$user_last\",\"first_name\":\"$user_first\",\"username\":\"$user_user\"}")
+echo $url1
+JSON_PUT1=$(curl -X PUT $url1 -H "Content-Type: application/json" -d "{\"id\":\"$user_id\",\"email\":\"john.snow83@gmail.com\",\"last_name\":\"$user_last\",\"first_name\":\"$user_first\",\"username\":\"$user_user\"}")
 
 echo $JSON_PUT1 | jq .
 
@@ -26,6 +26,9 @@ JSON_POST3=$(curl -X POST http://localhost:8080/api/exercises -H "Content-Type:a
 
 echo $JSON_POST3 | jq .
 
+JSON_POST4=$(curl -X POST http://localhost:8080/api/exercises -H "Content-Type:appication/json" -d "{\"name\":\"inside pendulum circle\",\"tool\":\"clubbell\",\"user_id\":\"$user_id\"}")
+
+echo $JSON_POST4 | jq .
 
 JSON_GET=$(curl -X GET "http://localhost:8080/api/users/$user_id/exercises" -H "Content-Type:application/json" -d "{\"user_id\":\"$user_id\"}")
 
@@ -38,3 +41,11 @@ curl -X DELETE "http://localhost:8080/api/exercises/$exercise_id"
 curl -X GET "http://localhost:8080/api/users/$user_id/exercises" | jq .
 
 curl -X GET "http://localhost:8080/api/users/$user_id" | jq .
+
+exercise_id=$(echo $JSON_POST4 | jq -r .id)
+
+url2="http://localhost:8080/api/exercises/$exercise_id"
+
+JSON_PUT2=$(curl -X PUT $url2 -H "Content-Type: application/json" -d "{\"name\":\"inside pendulum\",\"tool\":\"clubbell\"}")
+
+echo $JSON_PUT2 | jq .
