3b6e33cd242bb51ea57b37497334c98a48bceddd
Alexander Hansen <ahansen1335@gmail.com>
2025-02-20 06:20:04 +0100

    add workouts_exercises schema to link exercises to workouts

---
+++ sql/schema/004_workouts_exercises.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE workouts_exercises (
+    id UUID PRIMARY KEY,
+    time_seconds INTEGER NOT NULL,
+    workout_id UUID NOT NULL REFERENCES workouts (id) ON DELETE CASCADE,
+    exercise_id UUID NOT NULL REFERENCES exercises (id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE workouts_exercises;
