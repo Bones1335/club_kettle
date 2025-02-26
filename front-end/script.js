48c2beabeae39b942d72c15f37bb63be8ca08978
Alexander Hansen <ahansen1335@gmail.com>
2025-02-26 09:14:34 +0100

    create exercise form now adds the exercise to the database.

--- front-end/script.js
+++ front-end/script.js
@@ -12,7 +12,7 @@ function convertUserToJson() {
     return JSON.stringify(formData);
 }    
 
-async function sendData(jsonData) {
+async function sendUserData(jsonData) {
     let url = URL + "api/users"
     try {
         const response = await fetch(url, {
@@ -24,7 +24,6 @@ async function sendData(jsonData) {
         });
 
         const json = await response.json()
-        console.log(json)
         let jsonOutput = document.getElementById("jsonOutput");
         jsonOutput.innerHTML = `<pre>${json.last_name}, ${json.first_name}, ${json.id}, ${json.email}, ${json.username} </pre>`;
     }
@@ -33,25 +32,26 @@ async function sendData(jsonData) {
     }
 }
 
-function submitFormData() {
-    JSONData = convertUserToJson();
-    sendData(JSONData)
+function submitUserFormData() {
+    let JSONData = convertUserToJson();
+    sendUserData(JSONData)
 }
 
 function mySubmitFunction(e) {
     e.preventDefault();
 }
 
-async function getUsers() {
+/*async function getUsers() {
     let url = URL + "/api/users"
     const response = await fetch(url);
     const users = await response.json();
     document.getElementById('output').textContent = JSON.stringify(users, null, 2);
 }
+*/
 
 function addExerciseForm() {
     const formContainer = document.createElement("div");
-    formContainer.id = `exerciseFromContainer`;
+    formContainer.id = `exerciseFormContainer`;
 
     formContainer.innerHTML = `        
      <form id="exerciseData" onsubmit="mySubmitFunction(event)" method="post">
@@ -61,10 +61,10 @@ function addExerciseForm() {
         <label for="tool">Exercise Tool:</label>
         <input type="text" name="tool" id="tool">
 
-        <label for="username">User Who Created Exercise:</label>
-        <input type="text" name="username" id="username">
+        <label for="user_id">User Who Created Exercise:</label>
+        <input type="text" name="user_id" id="user_id">
 
-        <button type="submit" onclick="submitFormData()">Submit</button>
+        <button type="submit" onclick="submitExerciseFormData()">Submit</button>
         
     </form>
     `;
@@ -84,3 +84,29 @@ function convertExerciseToJson() {
     }
     return JSON.stringify(formData);
 } 
+
+async function sendExerciseData(jsonData) {
+    let url = URL + "api/exercises"
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: jsonData,
+        });
+
+        const json = await response.json()
+        console.log(json)
+        let jsonOutput = document.getElementById("exercises");
+        jsonOutput.innerHTML = `<pre>${json.name}, ${json.tool}, ${json.id}, ${json.user_id} </pre>`;
+    }
+    catch (error) {
+        console.error('Error:', error)
+    }
+}
+
+function submitExerciseFormData() {
+    let jsonData = convertExerciseToJson();
+    sendExerciseData(jsonData);
+}
