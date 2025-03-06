c3d91e6e0616783ba014adc0e1d2edb536c87f45
Alexander Hansen <ahansen1335@gmail.com>
2025-03-06 17:19:13 +0100

    add workout routines curl tests with updated resets

--- handler_reset.go
+++ handler_reset.go
@@ -8,7 +8,10 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Forbidden"))
 	}
 
-	cfg.db.Reset(r.Context())
+	cfg.db.ResetUsers(r.Context())
+	cfg.db.ResetExercises(r.Context())
+	cfg.db.ResetWorkoutRoutines(r.Context())
+	cfg.db.ResetWorkoutExercises(r.Context())
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("database reset to initial state\n"))
 }
