e2629b149446cf82ab34e8a2d3249612dfafcbc4
Alexander Hansen <ahansen1335@gmail.com>
2025-07-05 07:39:56 +0200

    add workout services

---
+++ front-end/js/services/workouts.js
@@ -0,0 +1,29 @@
+import { apiClient } from '../api/client.js';
+
+export class WorkoutService {
+    constructor() {
+        this.currentWorkout = null;
+        this.workoutStartTime = null;
+        this.durationInterval = null;
+    }
+
+    async createWorkouts(workoutData) {
+        return await apiClient.post('/workouts', workoutData);
+    }
+
+    async getWorkouts() {
+        return await apiClient.get('/workouts');
+    }
+
+    async getWorkout(id) {
+        return await apiClient.get(`/workouts/${id}`);
+    }
+
+    async updateWorkout(id, workoutData) {
+        return await apiClient.put(`/workouts/${id}`, workoutData);
+    }
+
+    async deleteWorkout(id) {
+        return await apiClient.delete(`workouts/${id}`);
+    }
+}
