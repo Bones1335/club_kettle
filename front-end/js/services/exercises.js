b8f5d91491dcd65adc3129b5f87980db98823d87
Alexander Hansen <ahansen1335@gmail.com>
2025-07-05 07:22:17 +0200

    add exercises service

---
+++ front-end/js/services/exercises.js
@@ -0,0 +1,20 @@
+import { apiClient } from '../api/client.js';
+
+export class ExerciseService {
+    async createExercises(exerciseData) {
+        return await apiClient.post('/exercises', exerciseData);
+    }
+    async getExercises() {
+        return await apiClient.get('/exercises');
+    }
+
+    async updateExercise(id, exerciseData) {
+        return await apiClient.put(`/exercises/${id}`, exerciseData);
+    }
+
+    async deleteExercise(id) {
+        return await apiClient.delete(`exercises/${id}`);
+    }
+}
+
+export const exerciseService = new ExerciseService();
